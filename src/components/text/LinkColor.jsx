4b999732b2dccdcda07f96a1688aa3b28d01b1f7
Kaivela <kaivel100@gmail.com>
2023-10-09 09:54:38 +0200

    applied changes

--- src/components/text/LinkColor.jsx
+++ src/components/text/LinkColor.jsx
@@ -1,17 +1,14 @@
 import { NavLink } from 'react-router-dom';
 
 export default function LinkColor(props) {
-  const customProps = Object.assign({}, props);
-  const className = props.className;
-  delete customProps.className;
   return (
     <NavLink
+      {...props}
       className={({ isActive }) => {
         return isActive
-          ? `${className} text-[#FFC500] m-2`
-          : `${className} text-white m-2`;
+          ? `${props.className} text-[#FFC500] m-2`
+          : `${props.className} text-white m-2`;
       }}
-      {...customProps}
     />
   );
 }
