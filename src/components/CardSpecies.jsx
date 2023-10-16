463077e34bd7906b50944fac5a249c8c193e6534
Kaivela <kaivel100@gmail.com>
2023-10-16 09:33:34 +0200

    changes for full CSS

--- src/components/CardSpecies.jsx
+++ src/components/CardSpecies.jsx
@@ -1,12 +1,18 @@
-export default function CardSpecies({ addClass, children }) {
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function CardSpecies({ children }) {
   return (
     <div
       className={
-        addClass +
-        ' scale-100 duration-300 hover:ml-10 hover:mr-10 hover:scale-125'
+        'card-species scale-100 duration-300 hover:ml-10 hover:mr-10 hover:scale-125'
       }
     >
       {children}
     </div>
   );
 }
+
+CardSpecies.propTypes = {
+  children: PropTypes.element.isRequired,
+};
--- src/components/pages/Species.jsx
+++ src/components/pages/Species.jsx
@@ -1,26 +1,14 @@
-import { useState } from 'react';
+import React from 'react';
 import CardSpecies from '../CardSpecies';
 import Smallcard from '../SmallCard';
 
 export default function Species() {
-  const [hoverIndex, setHoverIndex] = useState(null);
-
   const cards = [1, 2, 3, 4, 5];
 
-  const cardsIndex = cards.map((x, i) => (
-    <div
-      key={i}
-      onMouseEnter={() => setHoverIndex(i)}
-      onMouseLeave={() => setHoverIndex(null)}
-    >
-      <CardSpecies
-        addClass={
-          i + 1 === hoverIndex || i - 1 === hoverIndex ? 'scale-110' : ''
-        }
-      >
-        <Smallcard />
-      </CardSpecies>
-    </div>
+  const cardsIndex = cards.map((x) => (
+    <CardSpecies key={x}>
+      <Smallcard />
+    </CardSpecies>
   ));
 
   return <div className="mt-10 flex justify-center gap-5">{cardsIndex}</div>;
