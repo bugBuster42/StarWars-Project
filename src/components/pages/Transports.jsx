67645cfe6cdf77ac07a447cdb95a6420cecc57c9
Noemie.BARRE <noemie.barre24@gmail.com>
2023-10-24 17:19:56 +0200

    add myStar to the transport page

--- src/components/pages/Transports.jsx
+++ src/components/pages/Transports.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import Transport from '../TransportPage';
+import MovingStar from '../MovingStar';
+import Star from '../Star';
 
 export default function Transports() {
   const [transport, setTransport] = useState(true);
@@ -36,6 +38,14 @@ export default function Transports() {
           <Transport object="vehicles" />
         )}
       </div>
+      <MovingStar top="50px" width="20" />
+      <MovingStar top="500px" delay={6} />
+      <MovingStar top="800px" width="28" delay={2} />
+      <MovingStar top="750px" width="14" delay={10} />
+      <Star width={28} bottom="40" left="28" />
+      <Star width={14} top="32" right="56" />
+      <Star width={14} bottom="80" right="96" />
+      <Star width={20} top="96" left="96" />
     </>
   );
 }
