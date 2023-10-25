6267873313eed86423370aa84a682c0dbadda727
Noemie.BARRE <noemie.barre24@gmail.com>
2023-10-25 15:08:49 +0200

    change a ittle misatke

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import FilmsFooter from './FilmsFooter';
 export default function Footer() {
   return (
     <>
-      <footer className="z-[100] mt-44 h-20 w-screen border-t-2 border-primary before:absolute before:h-full before:w-full before:backdrop-blur">
+      <footer className="z-[100] mt-44 h-20 w-screen border-t-2 border-primary before:absolute before:h-[78px] before:w-full before:backdrop-blur">
         <div className="relative bottom-[60px] flex justify-center ">
           <FilmsFooter />
         </div>
