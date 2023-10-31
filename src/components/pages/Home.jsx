9703fa0de559a75fbce40da0d1ca6bd46c5e57eb
thomaslavalade <thomaslavalade@gmail.com>
2023-10-31 11:04:31 +0100

    links added, loading + animate move cards modified

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import getInfo from '../fetch/getInfo';
 import Loading from '../Loading';
 import HomeScrollingText from '../HomeScrollingText';
@@ -7,10 +8,22 @@ import Star from '../Star';
 
 const categories = {
   people: 83,
-  planets: 60,
+  species: 37,
   starships: 43,
   vehicles: 39,
-  species: 37,
+  planets: 60,
+};
+
+const getCategoryUrl = (category) => {
+  switch (category) {
+    case 'starships':
+    case 'vehicles':
+      return '/ships-vehicles';
+    case 'people':
+      return '/characters';
+    default:
+      return `/${category}`;
+  }
 };
 
 const checkImage = (url) =>
@@ -77,33 +90,45 @@ export default function Home() {
     };
   }, []);
 
-  if (loading) {
-    return <Loading />;
-  }
-
   return (
     <>
-      <HomeScrollingText />
-      <div className="animation-delay-40000 flex animate-move-cards flex-wrap justify-center gap-5 translate-y-[1vh] transform">
-        {Object.keys(data).map((category) => {
-          const item = data[category];
-          const id = item?.url ? item.url.split('/')[5] : null;
-          const imageCategory = category === 'people' ? 'characters' : category;
-          const imageUrl = `https://starwars-visualguide.com/assets/img/${imageCategory}/${id}.jpg`;
-          const name = item?.name || '';
+      <div className="relative">
+        {loading ? (
+          <div className="absolute -top-16 ml-72 flex justify-center">
+            <Loading />
+          </div>
+        ) : null}
 
-          return (
-            <div className key={category}>
-              <h1>{category}</h1>
-              <Smallcard
-                role={category}
-                name={name}
-                image={imageUrl}
-                fallback="/transport-placeholder.png"
-              />
-            </div>
-          );
-        })}
+        <HomeScrollingText />
+
+        <div className="animation-delay-39000 flex animate-move-cards flex-wrap justify-center gap-5 translate-y-[5vh] transform">
+          {Object.keys(data).map((category) => {
+            const item = data[category];
+            const id = item?.url ? item.url.split('/')[5] : null;
+            const imageCategory =
+              category === 'people' ? 'characters' : category;
+            const imageUrl = `https://starwars-visualguide.com/assets/img/${imageCategory}/${id}.jpg`;
+            const name = item?.name || '';
+
+            return (
+              <div
+                className="relative transition-transform duration-500 ease-in-out hover:-translate-y-5 hover:scale-105"
+                key={category}
+              >
+                <Link to={getCategoryUrl(category)}>
+                  <Smallcard
+                    name={name}
+                    image={imageUrl}
+                    fallback="/transport-placeholder.png"
+                  />
+                </Link>
+                <div className="absolute bottom-4 left-4 font-main text-3xl  font-semibold uppercase text-primary-dark">
+                  {category}
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
       <Star width="40" top="96" right="8" />
       <Star top="24" left="28" />
