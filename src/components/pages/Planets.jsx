228312b602cda31c46a0475df5a67fac66674959
Kaivela <kaivel100@gmail.com>
2023-11-02 15:40:39 +0100

    add loader when change pagination on planet page

--- src/components/pages/Planets.jsx
+++ src/components/pages/Planets.jsx
@@ -14,6 +14,7 @@ export default function Planets() {
   useEffect(() => {
     const controller = new AbortController();
     const url = `https://swapi.dev/api/planets/?page=${page}`;
+    setLoading(true);
     getInfo(url, controller)
       .then((data) => {
         setPlanets(data.results);
